23b0eeebd52a2cb943debd1aa77af53ca0c41789
DimasNahuat08 <43412856+DimasNahuat08@users.noreply.github.com>
2020-05-18 18:51:59 -0500

    .

---
+++ src/metodolagrange/MetodoLagrange.java
@@ -0,0 +1,17 @@
+
+package metodolagrange;
+
+
+ 
+public class MetodoLagrange {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmVentana v = new frmVentana();
+        v.setVisible(true);
+    }
+    
+}
